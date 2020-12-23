5a177c11e4641701697aa0fcb45001e22bdfaefd
UMUTONIRitha <rithamarie9@gmail.com>
2020-12-23 09:24:51 +0200

    add changes

---
+++ pictures/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+app_name = 'pictures'
+urlpatterns=[
+    url(r'^$',views.index,name='index'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^location/(?P<location>\w+)',views.image_location,name ='location')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
